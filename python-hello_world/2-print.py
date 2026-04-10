47ba0b6fe0e4ea13a31b298b339f6225ec0c7596
Aydinooo0208 <13758@holbertonstudents.com>
2026-04-10 11:48:25 +0000

    Add 2-print.py

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
+<<<<<<< HEAD
 print("Programming is like building a multilingual puzzle.")
+=======
+print("\"Programming is like building a multilingual puzzle\"")
+>>>>>>> 812e560 (nex)
